f20900e09a011ae69724db6ca255074e302a42f8
Daniel Steuri <daniel.steuri@flowingones.ch>
2025-08-13 18:44:53 +0200

    fix: resolve path relative to imported file

--- src/tile/mod.ts
+++ src/tile/mod.ts
@@ -2,83 +2,83 @@ import type { Theme } from "../mod.ts";
 
 const THEME_NAME = "tile";
 
-console.log(import.meta);
+console.log(import.meta.resolve("./css/styles.css"));
 
 export const tile: Theme = {
   stylesheets: [{
     name: `${THEME_NAME}/styles.css`,
-    content: await Deno.readFile(import.meta.dirname! + "/css/styles.css"),
+    content: await Deno.readFile(import.meta.resolve("./css/styles.css")),
     entrypoint: true,
   }, {
     name: `${THEME_NAME}/variables.css`,
-    content: await Deno.readFile(import.meta.dirname! + "/css/variables.css"),
+    content: await Deno.readFile(import.meta.resolve("./css/variables.css")),
     entrypoint: false,
   }, {
     name: `${THEME_NAME}/utils.css`,
-    content: await Deno.readFile(import.meta.dirname! + "/css/utils.css"),
+    content: await Deno.readFile(import.meta.resolve("./css/utils.css")),
     entrypoint: false,
   }, /* Layout */ {
     name: `${THEME_NAME}/header.css`,
-    content: await Deno.readFile(import.meta.dirname! + "/layout/header.css"),
+    content: await Deno.readFile(import.meta.resolve("./layout/header.css")),
     entrypoint: true,
   }, {
     name: `${THEME_NAME}/footer.css`,
-    content: await Deno.readFile(import.meta.dirname! + "/layout/footer.css"),
+    content: await Deno.readFile(import.meta.resolve("./layout/footer.css")),
     entrypoint: true,
   }, {
     name: `${THEME_NAME}/section.css`,
-    content: await Deno.readFile(import.meta.dirname! + "/layout/section.css"),
+    content: await Deno.readFile(import.meta.resolve("./layout/section.css")),
     entrypoint: true,
   }, /* Sections  */ {
     name: `${THEME_NAME}/hero.css`,
-    content: await Deno.readFile(import.meta.dirname! + "/sections/hero.css"),
+    content: await Deno.readFile(import.meta.resolve("./sections/hero.css")),
     entrypoint: true,
   }, {
     name: `${THEME_NAME}/image-banner.css`,
     content: await Deno.readFile(
-      import.meta.dirname! + "/sections/image-banner.css",
+      import.meta.resolve("./sections/image-banner.css"),
     ),
     entrypoint: true,
   }, {
     name: `${THEME_NAME}/benefits.css`,
     content: await Deno.readFile(
-      import.meta.dirname! + "/sections/benefits.css",
+      import.meta.resolve("./sections/benefits.css"),
     ),
     entrypoint: true,
   }, {
     name: `${THEME_NAME}/pricing.css`,
     content: await Deno.readFile(
-      import.meta.dirname! + "/sections/pricing.css",
+      import.meta.resolve("./sections/pricing.css"),
     ),
     entrypoint: true,
   }, {
     name: `${THEME_NAME}/integrations.css`,
     content: await Deno.readFile(
-      import.meta.dirname! + "/sections/integrations.css",
+      import.meta.resolve("./sections/integrations.css"),
     ),
     entrypoint: true,
   }, {
     name: `${THEME_NAME}/support.css`,
     content: await Deno.readFile(
-      import.meta.dirname! + "/sections/support.css",
+      import.meta.resolve("./sections/support.css"),
     ),
     entrypoint: true,
   }, {
     name: `${THEME_NAME}/contact.css`,
     content: await Deno.readFile(
-      import.meta.dirname! + "/sections/contact.css",
+      import.meta.resolve("./sections/contact.css"),
     ),
     entrypoint: true,
   }, /* Elements */ {
     name: `${THEME_NAME}/table.css`,
     content: await Deno.readFile(
-      import.meta.dirname! + "/table.css",
+      import.meta.resolve("./elements/table.css"),
     ),
     entrypoint: true,
   }, {
     name: `${THEME_NAME}/fader.css`,
     content: await Deno.readFile(
-      import.meta.dirname! + "/fader.css",
+      import.meta.resolve("./elements/fader.css"),
     ),
     entrypoint: true,
   }],
