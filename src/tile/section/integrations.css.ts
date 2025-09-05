1eaf758f9e92957f6ff9ad424cbdc243e47c8913
Daniel Steuri <daniel.steuri@flowingones.ch>
2025-09-05 13:34:59 +0200

    fix: typo in css

--- src/tile/section/integrations.css.ts
+++ src/tile/section/integrations.css.ts
@@ -1,5 +1,5 @@
 export default `.tile__integrations-section {
-    background-color: var(--integrations-color-background, rgba(0, 0 ,0 , 0.05)));
+    background-color: var(--integrations-color-background, rgba(0, 0 ,0 , 0.05));
 }
 .tile__integrations__cta {
     padding-top: calc(var(--tile-spacing) * 10);
